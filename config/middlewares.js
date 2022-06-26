a7e09e8dccf9ca9380ffe0ce07b21e5866c46679
TheIllEar <theillear@gmail.com>
2022-06-26 20:15:17 +0300

    update middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,42 +9,18 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', `res.cloudinary.com`],
+          'media-src': ["'self'", 'data:', 'blob:', `res.cloudinary.com`],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  }
 ];
-// module.exports = [
-//   "strapi::errors",
-//   "strapi::cors",
-//   "strapi::poweredBy",
-//   "strapi::logger",
-//   "strapi::query",
-//   "strapi::body",
-//   "strapi::session",
-//   "strapi::favicon",
-//   "strapi::public",
-//   {
-//     name: "strapi::security",
-//     config: {
-//       contentSecurityPolicy: {
-//         useDefaults: true,
-//         directives: {
-//           "connect-src": ["'self'", "https:"],
-//           "img-src": [
-//             "'self'",
-//             "data:",
-//             "blob:",
-//             "storage.googleapis.com",
-//             "dl.airtable.com",
-//           ],
-//           "media-src": [
-//             "'self'",
-//             "data:",
-//             "blob:",
-//             "storage.googleapis.com",
-//             "dl.airtable.com",
-//           ],
-//           upgradeInsecureRequests: null,
-//         },
-//       },
-//     },
-//   },
-// ];
